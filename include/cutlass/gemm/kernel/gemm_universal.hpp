44244db50270e3fea8123eef41c2add41b0f9e06
Sun, Jiwei1 <jiwei1.sun@intel.com>
2025-08-19 23:22:12 +0800

    fix compiler error for fsycl-host-compiler=c++

--- include/cutlass/gemm/kernel/gemm_universal.hpp
+++ include/cutlass/gemm/kernel/gemm_universal.hpp
@@ -53,6 +53,12 @@ struct IsCutlass3ArrayKernel<ProblemShape, cute::void_t<typename ProblemShape::U
 
 ////////////////////////////////////////////////////////////////////////////////
 
+
+#if defined(SYCL_INTEL_TARGET)
+#include "cutlass/gemm/kernel/xe_gemm.hpp"
+#include "cutlass/gemm/kernel/xe_gemm_cooperative.hpp"
+#include "cutlass/gemm/kernel/xe_gemm_array_cooperative.hpp"
+#else
 #include "cutlass/gemm/kernel/sm70_gemm.hpp"
 #include "cutlass/gemm/kernel/sm90_gemm_tma.hpp"
 #include "cutlass/gemm/kernel/sm90_gemm_warpspecialized.hpp"
@@ -71,11 +77,6 @@ struct IsCutlass3ArrayKernel<ProblemShape, cute::void_t<typename ProblemShape::U
 #include "cutlass/gemm/kernel/sm100_gemm_array_tma_warpspecialized_mma_transform.hpp"
 #include "cutlass/gemm/kernel/sm100_sparse_gemm_tma_warpspecialized.hpp"
 #include "cutlass/gemm/kernel/sm120_gemm_tma_warpspecialized_cooperative_asymmetric_dma.hpp"
-
-#if defined(SYCL_INTEL_TARGET)
-#include "cutlass/gemm/kernel/xe_gemm.hpp"
-#include "cutlass/gemm/kernel/xe_gemm_cooperative.hpp"
-#include "cutlass/gemm/kernel/xe_gemm_array_cooperative.hpp"
 #endif
 
 ////////////////////////////////////////////////////////////////////////////////
